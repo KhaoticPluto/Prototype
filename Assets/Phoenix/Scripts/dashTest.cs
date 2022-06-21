593ed10700b8b1445a5fefda050f5d376ff15f39
Phoenix <phoenix.millar.020@gmail.com>
2022-06-21 15:10:52 +1200

    Dash and Movement Test
    testing out movement with a cube and dodge mechanic

---
+++ Assets/Phoenix/Scripts/dashTest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dashTest : MonoBehaviour
+{
+    private Rigidbody _rb;
+    [SerializeField]public float _dashSpeed;
+    [SerializeField]private float _dashTime;
+    public float startDashTime;
+    private Vector2 _dashingDir;
+    private bool _isDashing;
+    private bool _canDash;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _dashTime = startDashTime;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _canDash) //When pressing LeftShift and you can dash
+        {
+            _isDashing = true;
+            _canDash = false;
+            _dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (_dashingDir == Vector2.zero)
+            {
+                _dashingDir = new Vector2(transform.localScale.x, 0);
+            }
+        }
+
+        if (_isDashing) //Dashing Stops
+        {
+            _rb.velocity = _dashingDir.normalized * _dashSpeed;
+            return;
+        }
+    }
+
+
+}
