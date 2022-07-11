c88fcfbd70f1021288c081a759c1076f16ce9a90
xyndall <tyrel.ratima@gmail.com>
2022-07-11 14:30:00 +1200

    added 2 new enemies
    addded sunglasses  and demon

---
+++ Assets/Tyrell/EnemyAi/EnemyAoeRange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAoeRange : EnemyAiController
+{
+    public GameObject projectile;
+    int ProjectilesFired = 10;
+
+    public override void AttackPlayer()
+    {
+
+
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here
+            for (int i = 0; i < ProjectilesFired; i++)
+            {
+                Rigidbody rb = Instantiate(projectile, transform.position + Vector3.up, Quaternion.identity).GetComponent<Rigidbody>();
+
+                Vector3 ShootDirection = transform.forward;
+                ShootDirection.x += Random.Range(-0.5f, 0.5f);
+                ShootDirection.z += Random.Range(-0.5f, 0.5f);
+                
+                rb.transform.LookAt(player.transform);
+                rb.velocity = ShootDirection * 30f ;
+                //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            }
+
+            ///End of attack code
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+
+}
