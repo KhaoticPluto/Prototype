6bcb681800cbb32230696f095e6421642b4a8eea
Phoenix <phoenix.millar.020@gmail.com>
2022-10-11 15:06:55 +1300

    Guards with Different guns and animations
    Have Sniper and Shotgun and Guards associated with it

--- Assets/Tyrell/EnemyAi/EnemyScripts/RangedEnemyGuardVar.cs
+++ Assets/Tyrell/EnemyAi/EnemyScripts/RangedEnemyGuardVar.cs
@@ -21,6 +21,7 @@ public class RangedEnemyGuardVar : EnemyAiController
         {
             ///Attack code here
             animator.SetTrigger("isAttacking");
+
             Rigidbody rb = Instantiate(projectile, transform.position + Vector3.up, Quaternion.identity).GetComponent<Rigidbody>();
             rb.transform.LookAt(player.transform);
             rb.velocity = transform.forward * bulletVelocity;
